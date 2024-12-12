891fd6bd26fec36cf5f5f093b69e01f7a5b3ba28
Yashpal Ramakrishnaiah <ryashpal89@gmail.com>
2024-12-12 04:58:50 +0000

    Annotations by risk score analysis

--- scripts/remove_fasta_data_from_gff3.py
+++ scripts/remove_fasta_data_from_gff3.py
@@ -1,10 +1,10 @@
 if __name__ == "__main__":
     import os
     inDir = os.environ['GENOMICS_DATA_BASE'] + '/annotations/s_aureus/'
-    outDir = os.environ['GENOMICS_DATA_BASE'] + '/genome_nlp_tokens/sample'
+    outDir = os.environ['GENOMICS_DATA_BASE'] + '/annotations/s_aureus_gff3'
     for file in os.listdir(inDir):
         print('file: ', file)
-        if (file.endswith('.gff3') and (('AH19F045' in file) or ('AH18K016' in file) or ('AH21A010' in file))):
+        if file.endswith('.gff3'):
             with open((inDir + '/' + file), mode='r') as in_file, open((outDir + '/' + file), mode='w') as out_file:
                 for line in in_file.readlines():
                     if '##FASTA' in line:
